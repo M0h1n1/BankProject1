231b887a4dd1322b76cb360592bdb3bb1797c9c1
amohini099 <amohini099@gmail.com>
2024-02-29 01:55:17 -0500

    My little commit

--- src/test/java/Letskode.java
+++ src/test/java/Letskode.java
@@ -1,13 +1,14 @@
-import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.sql.Driver;
+//import java.sql.Driver;
 import java.util.Set;
 
 public class Letskode {
@@ -97,5 +98,32 @@ public class Letskode {
         driver.findElement(By.id("login-password")).sendKeys("abcabc");
         driver.findElement(By.id("login")).click();
         Thread.sleep(3000);
+    }
+
+    @Test
+    public void testSuccesfulLogin() throws InterruptedException {
+        driver.findElement(By.id("email")).sendKeys("test@email.com");
+        driver.findElement(By.id("login-password")).sendKeys("abcabc");
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(10000);
+        Assert.assertTrue(driver.findElement(By.xpath("//img[contains(@src,'default-user-profile-pic.png')]")).isDisplayed());
+    }
+
+    @Test
+    public void testErrorMessageForInvalidCredentials() throws InterruptedException {
+        driver.findElement(By.id("email")).sendKeys("test1@email.com");
+        driver.findElement(By.id("login-password")).sendKeys("abcabc");
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(By.id("incorrectdetails")).getText().trim(),"Incorrect login details. Please try again.");
+    }
+
+    @Test
+    public void testErrorMessageForBlankEmail() throws InterruptedException {
+        driver.findElement(By.id("email")).sendKeys("");
+        driver.findElement(By.id("login-password")).sendKeys("abcabc");
+        driver.findElement(By.id("login")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='form-group']//span[@class='error']")).getText().trim(),"The email field is required.");
     }
 }
